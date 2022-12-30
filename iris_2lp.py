545084a54d2d92e50dcc16583ae1f3cf1496dee3
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-12-30 13:42:33 +0100

    backpropagate dloss_w2

--- iris_2lp.py
+++ iris_2lp.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 
@@ -7,42 +6,68 @@ class Perceptron:
         self.num_features = num_features
         self.num_hidden_units = num_hidden_units
         self.num_classes = num_classes
-        self.weights_input_to_hidden = np.zeros((num_hidden_units, num_features))
-        self.weights_hidden_to_output = np.zeros((num_classes, num_hidden_units))
-        self.bias_hidden = np.zeros(num_hidden_units)
-        self.bias_output = np.zeros(num_classes)
+        self.W1 = np.zeros((num_hidden_units, num_features))
+        self.W2 = np.zeros((num_classes, num_hidden_units))
+        self.B1 = np.zeros(num_hidden_units)
+        self.B2 = np.zeros(num_classes)
 
-    def predict(self, x):
+    def predict(self, X):
         # Calculate activations of hidden units
-        hidden_activations = np.dot(self.weights_input_to_hidden, x) + self.bias_hidden
+        Z1 = np.dot(self.W1, X) + self.B1
+        self.Z1 = Z1
         # Apply activation function to hidden activations
-        hidden_output = self._sigmoid(hidden_activations)
+        A1 = self._sigmoid(Z1)
+        self.A1 = A1
         # Calculate activations of output units
-        output_activations = np.dot(self.weights_hidden_to_output, hidden_output) + self.bias_output
+        Z2 = np.dot(self.W2, A1) + self.B2
+        self.Z2 = Z2
         # Apply activation function to output activations
-        output = self._softmax(output_activations)
-        return output
+        A2 = self._softmax(Z2)
+        return A2
 
     def train(self, x, y, learning_rate=0.1, epochs=100):
         for epoch in range(epochs):
             for i in range(len(x)):
-                prediction = self.predict(x[i])
-                error = y[i] - prediction
+                Yh = self.predict(x[i])
+                loss = self._categorical_crossentropy(y[i], Yh)
+                dloss_Yh = self._dcategorical_crossentropy(y[i], Yh)
+                # dloss_Z2 = dloss_Yh * self._dsoftmax(self.Z2)
+                dloss_Z2 = np.dot(dloss_Yh, self._dsoftmax(self.Z2))
+                dLoss_A1 = np.dot(self.W2.T, dloss_Z2)
+                dloss_W2 = np.kron(dloss_Z2, self.A1).reshape(self.num_classes, self.num_hidden_units)
                 # Calculate activations of hidden units
-                hidden_activations = np.dot(self.weights_input_to_hidden, x[i]) + self.bias_hidden
+                hidden_activations = np.dot(self.W1, x[i]) + self.B1
                 # Apply activation function to hidden activations
                 hidden_output = self._sigmoid(hidden_activations)
                 # Update weights and biases
-                self.weights_hidden_to_output += (learning_rate * error * hidden_output[:, np.newaxis]).reshape(self.num_classes, self.num_hidden_units)
-                self.bias_output += learning_rate * error
-                self.weights_input_to_hidden += np.kron(learning_rate * hidden_output * (1 - hidden_output), x[i]).reshape(self.num_hidden_units, self.num_features)
-                self.bias_hidden += learning_rate * hidden_output * (1 - hidden_output)
+                self.W2 += (learning_rate * loss * hidden_output[:, np.newaxis]).reshape(self.num_classes,
+                                                                                         self.num_hidden_units)
+                self.B2 += learning_rate * loss
+                self.W1 += np.kron(learning_rate * hidden_output * (1 - hidden_output), x[i]).reshape(
+                    self.num_hidden_units, self.num_features)
+                self.B1 += learning_rate * hidden_output * (1 - hidden_output)
 
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
+    def _dsigmoid(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
+
     def _softmax(self, x):
         return (np.exp(x) / np.exp(x).sum())
+
+    def _dsoftmax(self, x):
+        s = self._softmax(x)
+        return np.diag(s) - np.outer(s, s)
+
+    def _categorical_crossentropy(self, y_true, y_pred):
+        # Ensure that y_pred is a valid probability distribution
+        y_pred = np.clip(y_pred, 1e-9, 1 - 1e-9)
+        # Calculate the negative log-likelihood of the true class
+        return -np.sum(y_true * np.log(y_pred))
+
+    def _dcategorical_crossentropy(self, y_true, y_pred):
+        return (y_pred - y_true) / y_true.shape
 
 
 from sklearn.preprocessing import MinMaxScaler
@@ -58,28 +83,14 @@ converter = {
     2: [0, 0, 1],
 }
 for label in y:
-    y_cat.append(converter[label])
+    y_cat.append(np.array(converter[label]))
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 # Split the dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.2)
 
 
-# from sklearn.preprocessing import OneHotEncoder
-# enc = OneHotEncoder(handle_unknown='ignore')
-# y_hot = enc.fit_transform(X, y)
-y_cat = []
-converter = {
-    0: [1, 0, 0],
-    1: [0, 1, 0],
-    2: [0, 0, 1],
-}
-for label in y:
-    y_cat.append(converter[label])
-
-
 perceptron = Perceptron(4, 16, 3)
-
 
 perceptron.train(X_train, y_train, learning_rate=0.01, epochs=40)
 
@@ -97,4 +108,4 @@ for pred, g_truth in zip(y_pred, y_test):
     else:
         F += 1
 
-print(T / (T+F))
+print(T / (T + F))
